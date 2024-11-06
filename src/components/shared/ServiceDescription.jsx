c0b60c3e75d784cf28a1c266276423d5708efe8b
faseehai <faseehaiapp@gmail.com>
2024-11-07 02:33:03 +0500

    add services animations

--- src/components/shared/ServiceDescription.jsx
+++ src/components/shared/ServiceDescription.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import MediaPlayer from "./MediaPlayer";
 
-function ServiceDescription({ description = "Add Description" }) {
+function ServiceDescription({
+  description = "Add Description",
+  videoPath = "",
+}) {
   return (
-    <p className="max-w-4xl mx-auto px-8 text-center text-lg mb-10" dir="rtl">
-      {description}
-    </p>
+    <>
+      {videoPath && (
+        <div className="w-52 h-52 m-auto" data-aos="fade-up">
+          <MediaPlayer src={videoPath} />
+        </div>
+      )}
+      <p
+        data-aos="fade-up"
+        className="max-w-4xl mx-auto px-8 text-center text-lg mb-10"
+        dir="rtl"
+      >
+        {description}
+      </p>
+    </>
   );
 }
 
